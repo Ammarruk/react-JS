be2c167739e2c056f67c083ba04fd1eb78b99bc0
Ammarruk <ammarruk2002@gmail.com>
2025-05-09 14:08:30 +0530

    new

--- my-app/src/list.tsx
+++ my-app/src/list.tsx
@@ -1,8 +1,8 @@
 function ProductList() {
   const products = [
-    { id: 1, name: "Earphone", description: "Sony headphones are the best", price: 5999 },
-    { id: 2, name: "Laptop", description: "HP laptop is the best laptop in the world", price: 79999 },
-    { id: 3, name: "Smartwatch", description: "Latest smartwatch with health tracking", price: 19999 },
+    { id: 1, name: "Earphone", description: "Experience premium sound quality with Sony headphones, designed for comfort, style, and superior audio performance. Whether for music, calls, or gaming, they offer clear sound, deep bass, and noise cancellation features for an immersive listening experience.", price: 5999 },
+    { id: 2, name: "Laptop", description: "HP laptops combine performance, reliability, and sleek design, making them ideal for work, study, and entertainment. With powerful processors, long battery life, and vibrant displays, HP offers a smooth computing experience for every need.", price: 79999 },
+    { id: 3, name: "Smartwatch", description: "The Apple Watch blends cutting-edge technology with elegant design, offering health tracking, fitness features, and seamless connectivity. Stay updated, monitor your health, and control your devices—all from your wrist.", price: 19999 },
   ];
 
   return (
@@ -13,7 +13,7 @@ function ProductList() {
           <div key={product.id} className="list-group-item list-group-item-action">
             <h4 className="mb-2 fw-bold">{product.name}</h4> 
             <p className="mb-2 text-secondary">{product.description}</p>
-            <p className="mb-0 text-success fw-semibold">Rps {product.price}</p>
+            <p className="mb-0 text-success fw-semibold">Rps{product.price}</p>
           </div>
         ))}
       </div>
